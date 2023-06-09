ddb1f60042491d28ef5f4caf0f2ec668906dcc43
Diogo <diogorafaellourencooliveira97@gmail.com>
2023-06-09 23:40:13 +0100

    Worked on domain

--- application/backend/security/src/security.gateway.ts
+++ application/backend/security/src/security.gateway.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import { Gateway } from './../../shared/src/abstracts/gateway.abstract';
+import { SecurityGatewayCollection } from './shared/collections/security-gateway.collection';
+import { UserGateway } from './gateways/user/user.gateway';
 
 export class SecurityGateway extends Gateway<Router> {
   public constructor() {
@@ -7,6 +9,7 @@ export class SecurityGateway extends Gateway<Router> {
   }
 
   public override subscribe(): Router {
+    this._router.use(SecurityGatewayCollection.USERS, () => new UserGateway().subscribe());
     return this._router;
   }
 }
