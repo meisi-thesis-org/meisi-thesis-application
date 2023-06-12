c9243c70d2212d17103552b21bfd0624add56a20
Diogo <diogorafaellourencooliveira97@gmail.com>
2023-06-12 01:54:09 +0100

    Worked on Angular SubModule

--- jest.config.ts
+++ jest.config.ts
@@ -16,6 +16,17 @@ export default {
         '<rootDir>/application/backend/**/__tests__/**/**/*.spec.ts'
       ],
       preset: 'ts-jest'
+    },
+    {
+      displayName: '@meisi-thesis/application-frontend',
+      testEnvironment: 'node',
+      transform: { '^.+\\.ts?$': 'jest-preset-angular' },
+      testMatch: [
+        '<rootDir>/application/frontend/src/**/**/*.spec.ts'
+      ],
+      preset: 'jest-preset-angular',
+      setupFilesAfterEnv: ['<rootDir>/application/frontend/setup-jest.ts'],
+      globalSetup: 'jest-preset-angular/global-setup'
     }
   ]
 };
