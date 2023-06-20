b16c73749eab45049b760d52fc3d47ff9f539090
Diogo <diogorafaellourencooliveira97@gmail.com>
2023-06-20 23:26:11 +0100

    Java initialize project

---
+++ backend/src/test/java/com/oliveira/meisi/thesis/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliveira.meisi.thesis.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
