b8814298eb4f9bff1e28657f204dc3831bbe41cf
Diogo <diogorafaellourencooliveira97@gmail.com>
2023-06-28 10:55:43 +0100

    Worked on Initializing

---
+++ jest.config.ts
@@ -0,0 +1,23 @@
+export default {
+  resetMocks: false,
+  rootDir: './',
+  coverageDirectory: '<rootDir>/coverage',
+  collectCoverageFrom: [
+    '<rootDir>/application/**/**/__tests__/**/**/*.ts'
+  ],
+  testPathIgnorePatterns: ['<rootDir>/node_modules'],
+  moduleFileExtensions: ['js', 'ts'],
+  coverageReporters: ['json', 'html'],
+  projects: [
+    {
+      displayName: '@meisi-thesis/application-backend',
+      testEnvironment: 'node',
+      transform: { '^.+\\.ts?$': 'ts-jest' },
+      testMatch: [
+        '<rootDir>/application/backend/**/__tests__/**/**/*.spec.ts'
+      ],
+      preset: 'ts-jest',
+      resetMocks: false
+    }
+  ]
+};
