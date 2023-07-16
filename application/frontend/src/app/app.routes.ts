74d29bb7d8b1c941024710c5246d2d0114749729
Diogo <diogorafaellourencooliveira97@gmail.com>
2023-07-16 16:18:18 +0100

    Reboot Vue

--- application/frontend/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { type Routes } from '@angular/router';
-import { LandingComponent } from './pages/landing/landing.component';
-
-export const routes: Routes = [
-  {
-    path: '',
-    component: LandingComponent
-  }
-];
--- application/frontend/src/app/core/services/storage/storage.abstract.ts
+++
@@ -1,6 +0,0 @@
-export abstract class Storage {
-  abstract fetch<T>(name: string): T;
-  abstract save<T>(name: string, data: T): void;
-  abstract delete(name: string): void;
-  abstract clear(): void;
-}
