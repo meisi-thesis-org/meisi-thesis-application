5252174dc50dcf0a677013e84590a2ee2b4fca4b
Diogo <diogorafaellourencooliveira97@gmail.com>
2023-08-12 14:46:27 +0100

    worked on workspaces

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: 'standard-with-typescript',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.base.json'
+  },
+  rules: {
+    semi: 'off',
+    '@typescript-eslint/semi': 'off'
+  }
+}
