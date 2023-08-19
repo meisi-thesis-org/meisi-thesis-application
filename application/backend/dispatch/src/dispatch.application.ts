f6c94160351f0f650667f724652030374372467c
Diogo <diogorafaellourencooliveira97@gmail.com>
2023-08-19 12:36:41 +0100

    Worked on dispatcher

--- application/backend/dispatch/src/dispatch.application.ts
+++ application/backend/dispatch/src/dispatch.application.ts
@@ -2,6 +2,8 @@ import Express, { type Application } from 'express';
 import { QueueProvider } from '@meisi-thesis/application-backend-shared/src/providers/queue.provider';
 import * as QueueProviders from '@meisi-thesis/application-backend-shared/src/providers/queue.provider.type'
 import { InternalServerException } from '@meisi-thesis/application-backend-shared/src/exceptions/internal-server.exception';
+import * as nodemailer from 'nodemailer';
+import { RandomProvider } from '../../shared/src/providers/random.provider';
 
 export class DispatchApplication {
   private readonly application: Application;
@@ -22,9 +24,46 @@ export class DispatchApplication {
 
       await queueProvider.consumeQueue(
         connectionURL,
-        QueueProviders.Collection.REGISTER_EXCEPTION,
-        (message) => {
-          if (message === null) throw new InternalServerException();
+        QueueProviders.Collection.REGISTER_EMAIL,
+        async (message) => {
+          if (message === null) {
+            await queueProvider.sendQueue(
+              connectionURL,
+              QueueProviders.Collection.REGISTER_EMAIL,
+              Buffer.from(JSON.stringify({
+                correlationUuid: new RandomProvider().randomUuid(),
+                URL: '/send-email',
+                cause: 'Missing message'
+              }))
+            ).catch(() => { throw new InternalServerException(); });
+
+            return;
+          }
+
+          const parsedMessage: Record<string, string | undefined> = JSON.parse(message.content.toString());
+
+          if (
+            parsedMessage.correlationUuid === undefined ||
+            parsedMessage.URL === undefined ||
+            parsedMessage.toEmail === undefined ||
+            parsedMessage.subject === undefined ||
+            parsedMessage.text === undefined
+          ) {
+            await queueProvider.sendQueue(
+              connectionURL,
+              QueueProviders.Collection.REGISTER_EMAIL,
+              Buffer.from(JSON.stringify({
+                correlationUuid: parsedMessage.correlationUuid,
+                URL: parsedMessage.URL,
+                cause: parsedMessage
+              }))
+            ).catch(() => { throw new InternalServerException(); });
+
+            return;
+          }
+
+          const transporter = nodemailer.createTransport({});
+          await transporter.sendMail({}).catch(() => {});
         }
       ).catch(() => { throw new InternalServerException(); });
     })
