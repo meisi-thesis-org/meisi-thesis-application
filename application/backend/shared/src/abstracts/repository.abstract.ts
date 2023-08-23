d5e1bb72909e77d498ffe753e64af7b2b01db76e
Diogo <diogorafaellourencooliveira97@gmail.com>
2023-08-23 17:20:31 +0100

    Worked on repo

--- application/backend/shared/src/abstracts/repository.abstract.ts
+++ application/backend/shared/src/abstracts/repository.abstract.ts
@@ -1,4 +1,5 @@
 export abstract class Repository<K, V> {
+  abstract findBulk (): Promise<V[]>;
   abstract findOneByUuid (uuid: K): Promise<V | undefined>;
   abstract createOne (entity: V): Promise<void>
 }
