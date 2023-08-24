59b68dd94131ac36d2efc809b90a4241bc600d15
Diogo <diogorafaellourencooliveira97@gmail.com>
2023-08-24 10:12:02 +0100

    Worked on tests

--- application/backend/security/src/gateways/user/user.service.spec.ts
+++ application/backend/security/src/gateways/user/user.service.spec.ts
@@ -8,6 +8,9 @@ import { UserEntity } from './domain/user.entity';
 import { RandomProvider } from '@meisi-thesis/application-backend-shared/src/providers/random.provider';
 import { UserDTO } from './domain/user.domain';
 import { NonFoundException } from '@meisi-thesis/application-backend-shared/src/exceptions/non-found.exception';
+import { SignUpRequest } from './requests/sign-up.request';
+import { ConflictException } from '@meisi-thesis/application-backend-shared/src/exceptions/conflict.exception';
+import { HashProvider } from '@meisi-thesis/application-backend-shared/src/providers/hash.provider';
 
 describe('UserService', () => {
   const instance = new UserService();
@@ -69,17 +72,72 @@ describe('UserService', () => {
   })
 
   describe('signUp', () => {
+    const dummyUuid = new RandomProvider().randomUUID();
+    const dummyEmail = 'dummyEmail';
+    const dummyUsername = 'dummyUsername';
+    const dummyPhoneNumber = 'dummyPhoneNumber';
+    const dummyAccessToken = 'dummyAccessToken';
+    const dummyFirstName = 'dummyFirstName';
+    const dummyLastName = 'dummyLastName';
+    const dummyDateBirth = new Date().toISOString();
+    const dummyCreatedAt = new Date().toISOString();
+    const dummyUpdatedAt = new Date().toISOString();
+
+    const userEntity = new UserEntity(
+      dummyUuid,
+      dummyEmail,
+      dummyUsername,
+      dummyPhoneNumber,
+      dummyAccessToken,
+      dummyFirstName,
+      dummyLastName,
+      dummyDateBirth,
+      dummyCreatedAt,
+      dummyUpdatedAt
+    );
+
     beforeEach(() => {
       vi.mock('user.state.repository', () => ({
         findUserByCredentials: vi.fn(),
         createOne: vi.fn()
       }))
+      vi.mock('@meisi-thesis/application-backend-shared/src/providers/hash.provider', () => ({
+        hash: vi.fn()
+      }))
     })
 
-    it('should return an UserDTO', () => {})
-    it('should have an ConflictException thrown because there is already an user created with provided request body', () => {})
-    it('should have an InternalServerException thrown because an error ocurred while calling HashProvider hash ', () => {})
-    it('should have an InternalServerException thrown because an error ocurred while calling repository findUserByCredentials', () => {})
-    it('should have an InternalServerException thrown because an error ocurred while calling repository create', () => {})
+    const signUpRequest = new SignUpRequest('dummyUsername', 'dummyEmail', 'dummyPhoneNumber');
+
+    it('should return an UserDTO', async () => {
+      vi.spyOn(UserStateRepository.prototype, 'findUserByCredentials').mockResolvedValue(undefined);
+      vi.spyOn(UserStateRepository.prototype, 'createOne').mockResolvedValue();
+
+      await expect(instance.signUp(signUpRequest)).resolves.toBeInstanceOf(UserDTO);
+    })
+
+    it('should have an ConflictException thrown because there is already an user created with provided request body', async () => {
+      vi.spyOn(UserStateRepository.prototype, 'findUserByCredentials').mockResolvedValue(userEntity);
+
+      await expect(async () => await instance.signUp(signUpRequest)).rejects.toThrow(ConflictException);
+    })
+
+    it('should have an InternalServerException thrown because an error ocurred while calling HashProvider hash ', async () => {
+      vi.spyOn(HashProvider.prototype, 'hash').mockRejectedValue(new InternalServerException());
+
+      await expect(async () => await instance.signUp(signUpRequest)).rejects.toThrow(InternalServerException);
+    })
+
+    it('should have an InternalServerException thrown because an error ocurred while calling repository findUserByCredentials', async () => {
+      vi.spyOn(UserStateRepository.prototype, 'findUserByCredentials').mockRejectedValue(InternalServerException);
+
+      await expect(async () => await instance.signUp(signUpRequest)).rejects.toThrow(InternalServerException);
+    })
+
+    it('should have an InternalServerException thrown because an error ocurred while calling repository create', async () => {
+      vi.spyOn(UserStateRepository.prototype, 'findUserByCredentials').mockResolvedValue(undefined);
+      vi.spyOn(UserStateRepository.prototype, 'createOne').mockRejectedValue(InternalServerException);
+
+      await expect(async () => await instance.signUp(signUpRequest)).rejects.toThrow(InternalServerException);
+    })
   })
 })
