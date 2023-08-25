ce53db343da0233733208f272da0650dc8bf3a0a
Diogo <diogorafaellourencooliveira97@gmail.com>
2023-08-25 21:38:10 +0100

    Worked on tests

--- application/backend/security/src/gateways/user/user.gateway.ts
+++ application/backend/security/src/gateways/user/user.gateway.ts
@@ -6,6 +6,8 @@ import { FindUserByUuidSchema } from './schema/find-user-by-uuid.schema';
 import { SignUpSchema } from './schema/sign-up.schema';
 import { SignInSchema } from './schema/sign-in.schema';
 import { RefreshAccessCodeSchema } from './schema/refresh-access-code.schema';
+import { AccessTokenGuard } from './guards/access-token.guard';
+import { RefreshTokenGuard } from './guards/refresh-token.guard';
 
 export class UserGateway extends Gateway<Router> {
   private readonly userController = new UserController();
@@ -14,13 +16,13 @@ export class UserGateway extends Gateway<Router> {
     super(Router());
   }
 
-  public subscribe (): Router {
-    this.router.get('/:uuid', SchemaValidator(FindUserByUuidSchema), async (request: Request, response: Response) => await this.userController.findUserByUuid(request, response));
+  public override subscribe (): Router {
+    this.router.get('/:uuid', AccessTokenGuard, SchemaValidator(FindUserByUuidSchema), async (request: Request, response: Response) => await this.userController.findUserByUuid(request, response));
     this.router.post('/sign-up', SchemaValidator(SignUpSchema), async (request: Request, response: Response) => await this.userController.signUp(request, response));
     this.router.put('/sign-in', SchemaValidator(SignInSchema), async (request: Request, response: Response) => await this.userController.signIn(request, response));
     this.router.put('/refresh-access-code', SchemaValidator(RefreshAccessCodeSchema), async (request: Request, response: Response) => await this.userController.refreshAccessCode(request, response))
-    this.router.put('/:uuid/refresh-tokens', SchemaValidator(RefreshAccessCodeSchema), async (request: Request, response: Response) => { await this.userController.refreshTokens(request, response); })
-    this.router.put('/:uuid/sign-out', async (request: Request, response: Response) => await this.userController.signOut(request, response));
+    this.router.put('/:uuid/refresh-tokens', RefreshTokenGuard, SchemaValidator(RefreshAccessCodeSchema), async (request: Request, response: Response) => { await this.userController.refreshTokens(request, response); })
+    this.router.put('/:uuid/sign-out', AccessTokenGuard, async (request: Request, response: Response) => await this.userController.signOut(request, response));
 
     return this.router;
   }
