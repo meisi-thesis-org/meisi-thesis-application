4cc3bc05dff552bfc67a2194dd96e82d3a3c44bd
Diogo <diogorafaellourencooliveira97@gmail.com>
2023-09-07 21:16:27 +0100

    Worked on book module

--- application/backend/commerce/book/src/book.controller.spec.ts
+++ application/backend/commerce/book/src/book.controller.spec.ts
@@ -1,5 +1,9 @@
-import { describe, expect, it } from 'vitest';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { BookController } from './book.controller';
+import { type Request, type Response } from 'express';
+import { RandomProvider } from '@meisi-thesis/application-backend-utilities-shared/src/providers/random.provider';
+import { type BookDTO } from './structs/book.domain';
+import { BookService } from './book.service';
 
 describe('BookController', () => {
   const instance = new BookController();
@@ -8,8 +12,136 @@ describe('BookController', () => {
     expect(instance).toBeInstanceOf(BookController);
   })
 
-  describe('findBooksByDossierUuid', () => {})
-  describe('findBookByUuid', () => {})
-  describe('createBook', () => {})
-  describe('updateBookByUuid', () => {})
+  const requestMock = {} as unknown as Request;
+  const responseMock = {} as unknown as Response;
+
+  function defineResponseMock<T> (jsonResponse: T): void {
+    responseMock.json = vi.fn().mockReturnValue(jsonResponse);
+    responseMock.status = vi.fn(() => responseMock)
+  }
+
+  const randomProvider = new RandomProvider();
+
+  const randomString = randomProvider.randomString(16);
+  const randomUuid = randomProvider.randomUUID();
+  const randomDateBirth = new Date().toISOString();
+
+  const bookDTO: BookDTO = {
+    uuid: randomUuid,
+    dossierUuid: randomUuid,
+    designation: randomString,
+    description: randomString,
+    visible: true,
+    active: true,
+    createdAt: randomDateBirth,
+    updatedAt: randomDateBirth
+  }
+
+  describe('findBookByUuid', () => {
+    beforeEach(() => {
+      requestMock.params = { ...requestMock.params, uuid: 'dummyUuid' }
+    })
+
+    async function callFindBookByUuid (): Promise<Response> {
+      return await instance.findBookByUuid(requestMock, responseMock)
+    }
+
+    it('should throw an exception because an error ocurred while making a service request', async () => {
+      vi.spyOn(BookService.prototype, 'findBookByUuid').mockRejectedValue({ getHttpCode: vi.fn() });
+      defineResponseMock(new Error())
+
+      await expect(callFindBookByUuid()).resolves.toEqual(new Error());
+    })
+
+    it('should return an BookDTO collection', async () => {
+      vi.spyOn(BookService.prototype, 'findBookByUuid').mockResolvedValue(bookDTO);
+      defineResponseMock(bookDTO);
+
+      await expect(callFindBookByUuid()).resolves.toEqual(bookDTO);
+    })
+  })
+
+  describe('findBooksByDossierUuid', () => {
+    beforeEach(() => {
+      requestMock.query = { ...requestMock.query, dossierUuid: 'dummyUserUuid' }
+    })
+
+    async function callFindBooksByDossierUuid (): Promise<Response> {
+      return await instance.findBooksByDossierUuid(requestMock, responseMock)
+    }
+
+    it('should throw an exception because an error ocurred while making a service request', async () => {
+      vi.spyOn(BookService.prototype, 'findBooksByDossierUuid').mockRejectedValue({ getHttpCode: vi.fn() });
+      defineResponseMock(new Error())
+
+      await expect(callFindBooksByDossierUuid()).resolves.toEqual(new Error());
+    })
+
+    it('should return a collection of BookDTO', async () => {
+      vi.spyOn(BookService.prototype, 'findBooksByDossierUuid').mockResolvedValue([bookDTO]);
+      defineResponseMock([bookDTO]);
+
+      await expect(callFindBooksByDossierUuid()).resolves.toEqual([bookDTO]);
+    })
+  })
+
+  describe('createBook', () => {
+    beforeEach(() => {
+      requestMock.body = {
+        ...requestMock.body,
+        dossierUuid: 'dummyUserUuid',
+        designation: 'dummyDesignation',
+        description: 'dummyDescription'
+      }
+    })
+
+    async function callCreateBook (): Promise<Response> {
+      return await instance.createBook(requestMock, responseMock)
+    }
+
+    it('should throw an exception because an error ocurred while making a service request', async () => {
+      vi.spyOn(BookService.prototype, 'createBook').mockRejectedValue({ getHttpCode: vi.fn() });
+      defineResponseMock(new Error())
+
+      await expect(callCreateBook()).resolves.toEqual(new Error());
+    })
+
+    it('should return a BookDTO', async () => {
+      vi.spyOn(BookService.prototype, 'createBook').mockResolvedValue(bookDTO);
+      defineResponseMock(bookDTO);
+
+      await expect(callCreateBook()).resolves.toEqual(bookDTO);
+    })
+  })
+
+  describe('updateBookByUuid', () => {
+    beforeEach(() => {
+      requestMock.params = { ...requestMock.params, uuid: 'dummyUuid' }
+      requestMock.body = {
+        ...requestMock.body,
+        designation: 'dummyDesignation',
+        description: 'dummyDescription',
+        visible: true,
+        active: true
+      }
+    })
+
+    async function callUpdateBookByUuid (): Promise<Response> {
+      return await instance.updateBookByUuid(requestMock, responseMock)
+    }
+
+    it('should throw an exception because an error ocurred while making a service request', async () => {
+      vi.spyOn(BookService.prototype, 'updateBookByUuid').mockRejectedValue({ getHttpCode: vi.fn() });
+      defineResponseMock(new Error())
+
+      await expect(callUpdateBookByUuid()).resolves.toEqual(new Error());
+    })
+
+    it('should return an BookDTO collection', async () => {
+      vi.spyOn(BookService.prototype, 'updateBookByUuid').mockResolvedValue(bookDTO);
+      defineResponseMock(bookDTO);
+
+      await expect(callUpdateBookByUuid()).resolves.toEqual(bookDTO);
+    })
+  })
 })
--- application/backend/commerce/book/src/book.service.spec.ts
+++ application/backend/commerce/book/src/book.service.spec.ts
@@ -1,3 +1,156 @@
-import { describe } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
+import { RandomProvider } from '@meisi-thesis/application-backend-utilities-shared/src/providers/random.provider';
+import { BookService } from './book.service';
+import { type BookEntity, type BookDTO } from './structs/book.domain';
+import { BookStateRepository } from './repositories/book-state.repository';
+import { NonFoundException } from '@meisi-thesis/application-backend-utilities-shared/src/exceptions/non-found.exception';
+import { InternalServerException } from '@meisi-thesis/application-backend-utilities-shared/src/exceptions/internal-server.exception';
+import { NetworkProvider } from '@meisi-thesis/application-backend-utilities-shared/src/providers/network.provider';
+import { ConflictException } from '@meisi-thesis/application-backend-utilities-shared/src/exceptions/conflict.exception';
 
-describe('BookService', () => {})
+describe('BookService', () => {
+  const instance = new BookService();
+
+  it('should have an instanceOf BookService', () => {
+    expect(instance).toBeInstanceOf(BookService)
+  })
+  const randomProvider = new RandomProvider();
+
+  const randomString = randomProvider.randomString(16);
+  const randomUuid = randomProvider.randomUUID();
+  const randomDateBirth = new Date().toISOString();
+
+  const bookDTO: BookDTO = {
+    uuid: randomUuid,
+    dossierUuid: randomUuid,
+    designation: randomString,
+    description: randomString,
+    visible: true,
+    active: true,
+    createdAt: randomDateBirth,
+    updatedAt: randomDateBirth
+  }
+
+  describe('findBookByUuid', () => {
+    async function callFindDossierByUuid (): Promise<BookEntity> {
+      return await instance.findBookByUuid({
+        uuid: randomUuid
+      })
+    }
+
+    it('should have returned BookDTO', async () => {
+      vi.spyOn(BookStateRepository.prototype, 'findBookByUuid').mockResolvedValue(bookDTO)
+      await expect(callFindDossierByUuid()).resolves.toEqual(bookDTO)
+    })
+
+    it('should throw a NonFoundException because Repository.findBookByUuid returned undefined ', async () => {
+      vi.spyOn(BookStateRepository.prototype, 'findBookByUuid').mockResolvedValue(undefined)
+      await expect(callFindDossierByUuid()).rejects.toThrow(NonFoundException)
+    })
+
+    it('should throw a InternalServerException because Repository.findBookByUuid threw InternalServerException ', async () => {
+      vi.spyOn(BookStateRepository.prototype, 'findBookByUuid').mockRejectedValue(new InternalServerException())
+      await expect(callFindDossierByUuid()).rejects.toThrow(InternalServerException)
+    })
+  })
+
+  describe('findBooksByDossierUuid', () => {
+    async function callFindDossierByUuid (): Promise<BookEntity[]> {
+      return await instance.findBooksByDossierUuid({
+        dossierUuid: randomUuid
+      })
+    }
+
+    it('should have returned a collection of BookDTO', async () => {
+      vi.spyOn(BookStateRepository.prototype, 'findBooksByDossierUuid').mockResolvedValue([bookDTO])
+      await expect(callFindDossierByUuid()).resolves.toEqual([bookDTO])
+    })
+
+    it('should throw a InternalServerException because Repository.findBooksByDossierUuid threw InternalServerException ', async () => {
+      vi.spyOn(BookStateRepository.prototype, 'findBooksByDossierUuid').mockRejectedValue(new InternalServerException())
+      await expect(callFindDossierByUuid()).rejects.toThrow(InternalServerException)
+    })
+  })
+
+  describe('createBook', () => {
+    async function callCreateDossier (): Promise<BookEntity> {
+      return await instance.createBook({
+        dossierUuid: randomUuid,
+        designation: randomString,
+        description: randomString
+      })
+    }
+
+    it('should resolve to be defined', async () => {
+      vi.spyOn(BookStateRepository.prototype, 'findBookByProps').mockResolvedValue(undefined)
+      vi.spyOn(NetworkProvider.prototype, 'doHttpRequest').mockResolvedValue({})
+      vi.spyOn(BookStateRepository.prototype, 'createBook').mockResolvedValue()
+
+      await expect(callCreateDossier()).resolves.toBeDefined()
+    })
+
+    it('should throw a NonFoundException because Repository.findBookByProps returned other than undefined ', async () => {
+      vi.spyOn(BookStateRepository.prototype, 'findBookByProps').mockResolvedValue(bookDTO)
+      vi.spyOn(NetworkProvider.prototype, 'doHttpRequest').mockResolvedValue({})
+      vi.spyOn(BookStateRepository.prototype, 'createBook').mockResolvedValue()
+      await expect(callCreateDossier()).rejects.toThrow(ConflictException)
+    })
+
+    it('should throw a InternalServerException because Repository.findBookByProps threw InternalServerException ', async () => {
+      vi.spyOn(BookStateRepository.prototype, 'findBookByProps').mockRejectedValue(new InternalServerException())
+      vi.spyOn(NetworkProvider.prototype, 'doHttpRequest').mockResolvedValue({})
+      vi.spyOn(BookStateRepository.prototype, 'createBook').mockResolvedValue()
+      await expect(callCreateDossier()).rejects.toThrow(InternalServerException)
+    })
+
+    it('should throw a InternalServerException because Repository.createBook threw InternalServerException ', async () => {
+      vi.spyOn(BookStateRepository.prototype, 'findBookByProps').mockResolvedValue(undefined)
+      vi.spyOn(NetworkProvider.prototype, 'doHttpRequest').mockResolvedValue({})
+      vi.spyOn(BookStateRepository.prototype, 'createBook').mockRejectedValue(new InternalServerException())
+      await expect(callCreateDossier()).rejects.toThrow(InternalServerException)
+    })
+  })
+
+  describe('updateBookByUuid', () => {
+    async function callUpdateDossierByUuid (): Promise<BookEntity> {
+      return await instance.updateBookByUuid({
+        uuid: randomUuid,
+        designation: randomString,
+        description: randomString,
+        visible: true,
+        active: true
+      })
+    }
+
+    it('should to equal BookDTO', async () => {
+      vi.spyOn(BookStateRepository.prototype, 'findBookByUuid').mockResolvedValue(bookDTO)
+      vi.spyOn(BookStateRepository.prototype, 'updateBookByUuid').mockResolvedValue(bookDTO)
+
+      await expect(callUpdateDossierByUuid()).resolves.toEqual(bookDTO)
+    })
+
+    it('should throw a NonFoundException because Repository.findBookByUuid returned undefined ', async () => {
+      vi.spyOn(BookStateRepository.prototype, 'findBookByUuid').mockResolvedValue(undefined)
+      vi.spyOn(BookStateRepository.prototype, 'updateBookByUuid').mockResolvedValue(bookDTO)
+      await expect(callUpdateDossierByUuid()).rejects.toThrow(NonFoundException)
+    })
+
+    it('should throw a InternalServerException because Repository.findBooksByDossierUuid threw InternalServerException ', async () => {
+      vi.spyOn(BookStateRepository.prototype, 'findBookByUuid').mockRejectedValue(new InternalServerException())
+      vi.spyOn(BookStateRepository.prototype, 'updateBookByUuid').mockResolvedValue(bookDTO)
+      await expect(callUpdateDossierByUuid()).rejects.toThrow(InternalServerException)
+    })
+
+    it('should throw a NonFoundException because Repository.updateBookByUuid returned undefined ', async () => {
+      vi.spyOn(BookStateRepository.prototype, 'findBookByUuid').mockResolvedValue(bookDTO)
+      vi.spyOn(BookStateRepository.prototype, 'updateBookByUuid').mockResolvedValue(undefined)
+      await expect(callUpdateDossierByUuid()).rejects.toThrow(NonFoundException)
+    })
+
+    it('should throw a InternalServerException because Repository.findBooksByDossierUuid threw InternalServerException ', async () => {
+      vi.spyOn(BookStateRepository.prototype, 'findBookByUuid').mockResolvedValue(bookDTO)
+      vi.spyOn(BookStateRepository.prototype, 'updateBookByUuid').mockRejectedValue(new InternalServerException())
+      await expect(callUpdateDossierByUuid()).rejects.toThrow(InternalServerException)
+    })
+  })
+})
