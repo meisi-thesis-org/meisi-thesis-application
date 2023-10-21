6e9877256ce02100f8c9f5de0623abc79e2fc7e1
Diogo Oliveira <diogorafaellourencooliveira97@gmail.com>
2023-10-21 01:06:16 +0100

    Small fixes on tests

--- application/backend/utilities/shared/src/providers/random.provider.spec.ts
+++ application/backend/utilities/shared/src/providers/random.provider.spec.ts
@@ -1,39 +1,10 @@
-import { describe, it, expect, beforeEach, vi } from 'vitest';
+import { describe, it, expect } from 'vitest';
 import { RandomProvider } from './random.provider';
-import * as crypto from 'crypto';
 
 describe('RandomProvider', () => {
   const instance = new RandomProvider();
 
   it('should have an instanceOf RandomProvider', () => {
     expect(instance).toBeInstanceOf(RandomProvider);
-  })
-
-  beforeEach(() => {
-    vi.mock('crypto', () => ({
-      ...vi.importActual('crypto'),
-      randomUUID: vi.fn(),
-      randomBytes: vi.fn()
-    }))
-  })
-
-  describe('randomUUID', () => {
-    beforeEach(() => {
-      vi.spyOn(crypto, 'randomUUID').mockReturnValue('dummyUuid' as crypto.UUID)
-    })
-
-    it('should provide a random uuid', () => {
-      expect(instance.randomUUID()).toBe('dummyUuid')
-    })
-  })
-
-  describe('randomBytes', () => {
-    beforeEach(() => {
-      vi.spyOn(crypto, 'randomBytes').mockReturnValue('dummyString' as any)
-    })
-
-    it('should provide a random string', () => {
-      expect(instance.randomString(64)).toBe('dummyString')
-    })
   })
 })
